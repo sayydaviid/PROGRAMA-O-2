94fc7e5ae919b5be1627d7992455df14d367366c
David Tavares <139977517+sayydaviid@users.noreply.github.com>
2024-04-02 21:28:34 -0300

    Add files via upload

--- javinha/Boleto.java
+++ javinha/Boleto.java
@@ -1,9 +1,14 @@
 public class Boleto {
-    double valor;
+    private double valor;
     String codigodebarras;
     String vencimento;
-    public void emitirBoleto(){
-        System.out.println("Emitindo novo boleto:" + valor +"," + vencimento + "," + codigodebarras); 
+    public void emitirBoleto( double valor,
+    String codigodebarras,
+    String vencimento){
+        System.out.println("Emitindo novo boleto:" + valor +"," + vencimento + "," + codigodebarras);
+        variavelBoleto.valor = valor;
+        variavelBoleto.vencimento = vencimento;
+        variavelBoleto.codigodebarras = codigodebarras ;
     }
        
     public void cancelarBoleto(){}
--- javinha/Impressao.java
+++ javinha/Impressao.java
@@ -1,6 +1,9 @@
 public class Impressao {
 
-    public void imprimir(){}
+    public void imprimir(Boleto variavelBoleto){
+        // variavelBoleto.valor = 100;
+        System.out.println("Finalmente Imprimindo...!" + variavelBoleto.valor);
+    }
 
     public void escanear(){}
 
